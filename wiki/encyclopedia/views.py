acfa580c309fdda9a371c2285e16f31c336992da
Jose Paralta <jeparalta@gmail.com>
2023-01-01 16:01:28 +0000

    Fixed urls

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,9 +5,11 @@ from . import util
 
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries()  
     })
+    
 
 def entry(request, title):
     
@@ -21,6 +23,9 @@ def entry(request, title):
         return render(request, "encyclopedia/entry.html", { 
         "title": title, "entry": entry
     })
+
+
+
 
 
 
